5f0ac861429457e3d5ada96280e4fbe1bcc70ce4
eldor-ergashov <eldor7147@gmail.com>
2022-04-24 18:56:11 +0500

    create: user slider

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
-const swiper = new Swiper('.swiper', {
+const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+
+  effect: 'slide',
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
   },
 
   effect: 'slide',
